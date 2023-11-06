ea64de5068d5cb71024bec5e1b409c85eeb682de
Gilbert Ardila <gilbertferney@gmail.com>
2023-11-06 17:04:22 -0500

    Error page done

---
+++ src/app/error.jsx
@@ -0,0 +1,23 @@
+'use client';
+import { useEffect } from "react";
+
+const Error = ({error, reset}) => {
+    
+useEffect(() => {
+ 
+console.log(error)
+ 
+}, [error])
+
+
+  return (
+    <div className="text-center mt-20">
+        <h1 className="text-xl text-center">Upsss, Something went wrong</h1>
+        <button 
+        className="mt-20 bg-amber-500 rounded-lg p-1 w-auto mx-auto hover:scale-110"
+        onClick={()=> reset()}>Try again</button>
+    </div>
+  )
+}
+
+export default Error;
