8429b2fd21d325fb08aebeb06079fe280d2a0bf2
Gilbert Ardila <gilbertferney@gmail.com>
2023-11-06 08:39:29 -0500

    About page done

---
+++ src/app/about/page.jsx
@@ -0,0 +1,26 @@
+import React from 'react'
+
+const About = () => {
+  return (
+    <div className='max-w-6xl mx-auto space-x-4'>
+        <h1 className='text-2xl font-medium text-amber-600'>About</h1>
+        <p>Lorem ipsum dolor, sit amet consectetur adipisicing elit. Ad impedit inventore, soluta minima aspernatur sequi rem repellendus facere ipsa cumque, incidunt eos sunt obcaecati saepe. Laudantium iste repudiandae fugiat voluptas!</p>
+        <br />
+        <p>Lorem ipsum dolor, sit amet consectetur adipisicing elit. Beatae ab sequi, vero exercitationem recusandae et neque repellendus mollitia perspiciatis omnis quam autem labore doloremque voluptatibus repellat. Alias consequuntur ipsa autem?
+        Ea, accusantium alias numquam laborum non, fuga architecto id voluptatum quaerat, est nemo asperiores. Quod itaque unde, maiores placeat odio magni fuga nobis voluptas. Dolore aliquam illo dicta impedit quia.
+        Et quod necessitatibus ea commodi ipsam quo dolorem aspernatur vero! Ab ducimus nemo sequi consequuntur obcaecati, doloremque non dolor reprehenderit asperiores. Fuga minima facere praesentium, minus necessitatibus voluptatem nulla recusandae.
+        Animi veritatis minus pariatur hic quis reiciendis doloribus nobis commodi veniam. Natus maxime iure tempore sed nobis corporis! Non adipisci animi placeat reprehenderit quisquam nulla aut odio excepturi voluptates sed.
+        </p>
+        <br />
+        <p>
+        Ex nemo nostrum saepe sequi maxime, ipsam beatae, repudiandae eum quaerat minus, tempore laudantium! Fugiat magnam vero quam aliquid est culpa dicta quia consectetur quasi corporis exercitationem, doloribus sapiente voluptates.
+        Aliquam accusantium eum repudiandae laborum ipsam repellat tenetur voluptates nobis sequi corrupti iusto impedit perferendis, in tempore nemo omnis mollitia suscipit libero facere eius et sapiente voluptatum. Voluptates, id. Provident!
+        Repudiandae perspiciatis voluptatibus aliquam, mollitia ducimus reiciendis natus? Assumenda iusto, perferendis voluptas est dignissimos consequatur architecto suscipit enim odit earum reiciendis deleniti. Maxime earum eaque blanditiis expedita, libero officia quam.
+        Ratione saepe ab tenetur similique veritatis ex veniam dolor culpa ipsum autem perspiciatis adipisci, voluptatum, numquam doloribus accusamus itaque explicabo! Praesentium sit necessitatibus mollitia, temporibus tempore excepturi fugiat nisi. Harum!
+        Consequatur nemo, architecto voluptate amet, nostrum alias praesentium neque unde aliquam, saepe eaque? Deserunt dolore nisi voluptatem maiores laborum culpa nam consequatur. Explicabo tenetur beatae quas ipsum perferendis vitae quos!
+        </p>
+    </div>
+  )
+}
+
+export default About;
