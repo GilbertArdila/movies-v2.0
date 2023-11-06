2c4e427f5679c51a12051d9f67e7a4aced8a6e6c
Gilbert Ardila <gilbertferney@gmail.com>
2023-11-06 17:16:17 -0500

    Loading page done

---
+++ src/app/loading.jsx
@@ -0,0 +1,11 @@
+import Image from "next/image";
+
+const Loading = () => {
+  return (
+    <div className="flex justify-center ">
+        <Image width={96} height={384} src='spinner.svg' alt='loading' />
+        </div>
+  )
+}
+
+export default Loading;
