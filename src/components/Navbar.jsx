90b74290b1823685ceeefcdd776d7664c0d8fd43
Gilbert Ardila <gilbertferney@gmail.com>
2023-11-09 18:46:00 -0500

    wrapping the NavbarItem component in a suspense boundary trying to avoid a deployment error

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,5 +1,10 @@
 import NavbarItem from './NavbarItem';
+import { Suspense } from 'react';
 
+
+function NavbarItemFallback(){
+    return <>placeholder</>
+}
 const Navbar = () => {
 
 
@@ -7,17 +12,19 @@ const Navbar = () => {
         <nav className='flex  justify-around min-w-full h-12 dark:bg-gray-500 bg-gray-300'>
             <ul className='flex self-center justify-between'>
                 <li className="mr-4">
+                    <Suspense fallback={<NavbarItemFallback/>}>
                     <NavbarItem title={'Trending'}
                         param='fetchTrending'
 
-                    />
+                    /></Suspense>
 
                 </li>
                 <li>
+                <Suspense fallback={<NavbarItemFallback/>}>
                     <NavbarItem title={'Top Rated'}
                         param='fetchTopRated'
 
-                    />
+                    /></Suspense>
                 </li>
             </ul>
         </nav>
