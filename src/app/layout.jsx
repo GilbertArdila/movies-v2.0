ff25536bba98ba5381222dbb95f4a0b54728ef6b
Gilbert Ardila <gilbertferney@gmail.com>
2023-11-09 19:49:17 -0500

    add use client to layout tryong to solve a deployment error

--- src/app/layout.jsx
+++ src/app/layout.jsx
@@ -1,13 +1,11 @@
+'use client';
 import Header from '@/components/Header';
 import Providers from './Providers';
 import Navbar from '@/components/Navbar';
 import SearchBox from '@/components/SearchBox';
 
 
-export const metadata = {
-  title: 'The movie App V2',
-  description: 'This the new movie App version',
-}
+
 
 export default function RootLayout({ children }) {
   return (
